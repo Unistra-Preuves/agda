a93d998d5a41757ae4dd305c91117de031b48a11
ewen575724 <ewen575724@gmail.com>
2026-06-12 17:17:34 +0200

    datatypes WIP
    need to add eliminators

--- src/full/Agda/Canonical/Canonical.hs
+++ src/full/Agda/Canonical/Canonical.hs
@@ -33,6 +33,8 @@ import Agda.Utils.Haskell.Syntax (DataOrNew(DataType))
 import Agda.Syntax.Translation.AbstractToConcrete (ToConcrete(toConcrete))
 import Agda.TypeChecking.Substitute (Apply(apply), piApply, domFromNamedArgName)
 import Data.Foldable (foldlM)
+import Data.ByteString.Lazy (fromStrict, toStrict )
+import Data.ByteString (packCString, useAsCString)
 
 foreign import ccall "canonical" canonical :: CString -> CString -> Word64 -> Word64 -> IO CString
 
@@ -40,9 +42,9 @@ typeToCType :: Type -> -- Type to convert
                [(String, Maybe CType)] ->  -- Pi bindings
                [(String, Maybe CType)] ->  -- lets bindings
                [String] ->  -- Names for variables
-               [QName] -> -- Already seen data types
+               [String] -> -- Already seen data types
                Bool -> -- toplevel?
-               TCM (CType, [QName], [(String, Maybe CType)]) -- Converted type along with all datatypes encountered
+               TCM (CType, [String], [(String, Maybe CType)]) -- Converted type along with all datatypes encountered
 typeToCType t = toCType (unEl t)
 
 toCSpine :: Term -> [String] -> TCM CSpine
@@ -62,18 +64,18 @@ toCType  :: Term ->
             [(String, Maybe CType)] ->
             [(String, Maybe CType)] ->
             [String] ->
-            [QName] ->
+            [String] ->
             Bool -> -- TopLevel ?
-            TCM (CType, [QName], [(String, Maybe CType)])
+            (TCM (CType, [String], [(String, Maybe CType)]))
 toCType t bds lts names alrdsn tplvl =
   case t of
     Pi a (NoAbs nb b ) -> do
-                (domty, alrdsn, lts) <- (typeToCType (unDom a) [] lts names alrdsn False)
-                typeToCType b ((nb , Just domty) : bds) lts names alrdsn  tplvl
+                (domty, alrdsn', lts') <- (typeToCType (unDom a) [] lts names alrdsn False) -- Convert domain type
+                typeToCType b ((nb , Just domty) : bds) lts' names alrdsn'  tplvl -- Convert  codomain type
     Pi a b -> do
-      (domty, alrdsn, lts) <- typeToCType (unDom a) [] lts names alrdsn False
-      typeToCType (unAbs b) ((absName b, Just domty) : bds) lts (absName b : names) alrdsn tplvl
-    Sort s ->
+      (domty, alrdsn', lts') <- typeToCType (unDom a) [] lts names alrdsn False
+      typeToCType (unAbs b) ((absName b, Just domty) : bds) lts' (absName b : names) alrdsn' tplvl
+    Sort s -> -- If get a Sort, we just return its name for now
       return (CType {
         bindings = reverse bds,
         lets = if tplvl then reverse lts else [],
@@ -82,10 +84,10 @@ toCType t bds lts names alrdsn tplvl =
             sargs = []
           }
       }, alrdsn, lts)
-    Def qname el ->
-      if (qname `elem` alrdsn ) then
-         return (CType {
-            bindings  = reverse bds,
+    Def qname el -> -- On defined names
+      if (((P.prettyShow <$> qnameName ) qname) `elem` alrdsn ) then -- if we already encountered the name,
+         return (CType {                                                         -- we already have gathered all its informations and put them in lts
+            bindings  = reverse bds,                                             -- we just give the type by its name
             lets = if tplvl then reverse lts else [],
             codom = CSpine {
                 shead = P.prettyShow (qnameName qname),
@@ -93,33 +95,36 @@ toCType t bds lts names alrdsn tplvl =
               }
           }, alrdsn, lts)
       else do
-          def <- getConstInfo qname
-          let ty = defType def -- Oublie pas de l'ajouter ça serait dommage
-          case theDef def of
-            DatatypeDefn DatatypeData { _dataCons = cons } -> do
-              defs <- mapM getConstInfo cons
+          def <- getConstInfo qname                                           -- gather its informations
+          let ty = defType def                                                -- get it's type
+          (tys, alr, letsss) <- (typeToCType ty [] lts names alrdsn False)    -- convert it and add it to already seen types
+          let letss = (P.prettyShow $ qnameName qname , Just tys) : letsss
+          case theDef def of                                                  -- match on the kind of definition we have for the type
+            DatatypeDefn DatatypeData { _dataCons = cons } -> do              -- get all the type constructors names
+              let alrdsn' = (P.prettyShow <$> qnameName) qname : alr            -- we add the new encoutered type in the list
+              defs <- mapM getConstInfo cons                                  -- get their informations
               let tys = map defType defs
-              (ctys, alrdsn, lts) <- foldlM (\(acc, alrdsn, lts) t -> do
-                                      (nt, alrdsn, lts) <- typeToCType t [] lts names alrdsn False
-                                      return (nt : acc, alrdsn, lts))
-                                     ([], qname : alrdsn, lts) tys
-              let lts = (zip (map (P.prettyShow <$> qnameName) cons) (map Just ctys) ) ++ lts
+              (ctys, alrdsnes, lts'') <- foldlM (\(acc, alrdsn', lets) t -> do
+                                      (nt, alrdsns', lets') <- typeToCType t [] lets names alrdsn' False -- (cpt + 1000)
+                                      return (nt : acc, alrdsns', lets'))
+                                     ([], alrdsn', letss) tys
+              let lteess = (reverse $ (zip (map (P.prettyShow <$> qnameName) cons) (map Just (reverse ctys)) ) ) ++ lts''
               return  (CType {
                   bindings  = reverse bds,
-                  lets = if tplvl then reverse lts else [],
+                  lets = if tplvl then reverse lteess else [],
                   codom = CSpine {
-                      shead = P.prettyShow (qnameName  qname),
+                      shead = (P.prettyShow $ qnameName qname ), -- show ctys ++ "tys : " ++ P.prettyShow tys, -- (qnameName qname),
                       sargs = []
                     }
-                }, qname : alrdsn, lts)
+                }, alrdsnes, lteess)
             d -> return (CType {
                 bindings = reverse bds,
-                lets = if tplvl then reverse lts else [],
+                lets = if tplvl then reverse letss else [],
                 codom  = CSpine {
                     shead  = P.prettyShow d,
                     sargs  = []
                   }
-              }, qname : alrdsn, lts)
+              }, alr, letss)
     _ -> do
       codom <- toCSpine t names
       return (CType {
@@ -127,6 +132,13 @@ toCType t bds lts names alrdsn tplvl =
         lets = if tplvl then reverse lts else [],
         codom
       }, alrdsn , lts)
+
+
+-- myZip :: [String] -> [Maybe CType] -> [(String, Maybe CType)]
+-- myZip [] [] = []
+-- myZip (s : ss) (t : tt) = (s , t) : (myZip ss tt)
+-- myZip [] (_ : _) = __IMPOSSIBLE__
+-- myZip (_ : _) [] = __IMPOSSIBLE__
 
 
 elimsToCterm :: [String] -> Elim' Term -> TCM CTerm
@@ -170,7 +182,7 @@ produceCanonicalGoal :: Telescope -> Type -> TCM CType
 produceCanonicalGoal ctx ty = aux ctx ty [("Set", Nothing)] [] [] []
   where
 
-  aux :: Telescope -> Type -> [(String, Maybe CType)] -> [(String, Maybe CType)] -> [String] -> [QName] ->  TCM CType
+  aux :: Telescope -> Type -> [(String, Maybe CType)] -> [(String, Maybe CType)] -> [String] -> [String] ->  TCM CType
   aux ctx ty revLets revPis names alrdsn =
     case ctx of
       EmptyTel -> do
@@ -179,8 +191,8 @@ produceCanonicalGoal ctx ty = aux ctx ty [("Set", Nothing)] [] [] []
       ExtendTel dom (Abs nb b) ->
         case unEl ty of
           Pi _ codom -> do
-            (domty, alrdsn, lts) <- (typeToCType (unDom dom) [] revLets names alrdsn False)
-            aux b (unAbs codom) ((nb, Just domty) : lts ) revPis (nb : names) alrdsn
+            (domty, alrdsns, lts') <- (typeToCType (unDom dom) [] revLets names alrdsn False )
+            aux b (unAbs codom) ((nb, Just domty) : lts' ) revPis (nb : names) alrdsns
           _ -> return dummyCType -- Is this impossible ?
       _ -> __IMPOSSIBLE__
 
@@ -192,10 +204,10 @@ call_canonical norm ii rng args = do --withInteractionId ii $ do
     getMetaTypeInContext metaId
   ctx <- liftTCM $ withInteractionId ii getContextTelescope
   goal <- liftTCM $ (produceCanonicalGoal ctx ty)
-  liftIO $ do
-    ety <- newCString (show $ encode goal) --
+  liftIO $
+    useAsCString (toStrict (encode goal)) $ \ety -> do -- may be dangerous, have to check
     name <- newCString "proof"
     res <- canonical ety name 1000 1
-    fstr <- peekCString res
-    fres :: CTerm <- liftMaybe (decode (fromString  fstr))
-    return (CanonicalExpr (show fres))
+    fstr <- packCString res
+    fres :: CTerm <- liftMaybe (decode (fromStrict  fstr))
+    return (CanonicalExpr (show fres ))
