d23cb11691c87a50f6d00d81b695eeaa76bec1a1
ewen575724 <ewen575724@gmail.com>
2026-07-08 19:28:20 +0200

    wip "fix Canonical changes"

--- src/full/Agda/Canonical/Canonical.hs
+++ src/full/Agda/Canonical/Canonical.hs
@@ -45,136 +45,135 @@ foreign import ccall "canonical" canonical :: CString -> CString -> Word64 -> Wo
 nameToString :: QName -> String
 nameToString = P.prettyShow <$> qnameName
 
-typeToCType :: Type -> -- Type to convert
-               [(String, Maybe CType)] ->  -- Pi bindings
-               [(String, Maybe CType, [CRule])] ->  -- lets bindings
+typeToCDecl :: Type -> -- Type to convert
+               [CDecl] ->  -- Pi bindings
+               [CDecl] ->  -- lets bindings
                [String] ->  -- Names for variables
                Map String Type -> -- Already seen data types
                Bool -> -- toplevel?
-               TCM (CType, Map String Type, [(String, Maybe CType, [CRule])]) -- Converted type along with all datatypes encountered
-typeToCType t = toCType (unEl t)
+               TCM (CDecl, Map String Type, [CDecl]) -- Converted type along with all datatypes encountered
+typeToCDecl t = toCDecl (unEl t)
 
-toCSpine :: Term -> [String] -> [(String, Maybe CType, [CRule])] -> Map String Type -> TCM (CSpine, Map String Type, [(String, Maybe CType, [CRule])])
+toCSpine :: Term -> [String] -> [CDecl] -> Map String Type -> TCM (CSpine, Map String Type, [CDecl])
 toCSpine t names lts alrdsn =
   case t of
     Var i e -> do
-      (sargs, alrdsn', lts') <-  elimsToCterm e names lts alrdsn
+      (args, alrdsn', lts') <-  elimsToCExpr e names lts alrdsn
       return (CSpine {
-        shead =  names !! i ,
-        sargs
+        head =  names !! i ,
+        args
       }, alrdsn', lts')
     Def qname e -> do
-      (sargs, alrdsn', lets') <- elimsToCterm  e names lts alrdsn
+      (args, alrdsn', lets') <- elimsToCExpr  e names lts alrdsn
       (lt, ald) <- gatherDatatypeInformations qname lets' alrdsn' names
       return (CSpine {
-          shead = P.prettyShow (qnameName qname),
-          sargs
+          head = P.prettyShow (qnameName qname),
+          args
         }, ald, lt)
     Con hd _ e -> do
       infos <- getConstInfo (conName hd)
       let (dataname, pars) = case theDef infos of
                     ConstructorDefn cd -> (_conData cd, _conPars cd)
                     _ -> __IMPOSSIBLE__
-      (sargs, alrdsn', lts') <- elimsToCterm e names lts alrdsn
+      (args, alrdsn', lts') <- elimsToCExpr e names lts alrdsn
       (lt, ald) <- gatherDatatypeInformations dataname lts' alrdsn' names
       return (CSpine {
-          shead = P.prettyShow . qnameName $ conName hd,
-          sargs
+          head = P.prettyShow . qnameName $ conName hd,
+          args
         }, ald, lt)
     Sort (Type l) -> do
       t' <- reallyUnLevelView l
-      (arg, alrdsn', lts') <- toCTerm t' names lts alrdsn
+      (arg, alrdsn', lts') <- toCExpr t' names lts alrdsn
       return (CSpine {
-          shead = "Set",
-          sargs = [arg]
+          head = "Set",
+          args = [arg]
         }, alrdsn', lts')
     Sort s ->
       return (CSpine {
-          shead = P.prettyShow s,
-          sargs = []
+          head = P.prettyShow s,
+          args = []
         }, alrdsn , lts)
     Level l -> do
       t' <- reallyUnLevelView l
       toCSpine t' names lts alrdsn
     Pi{} -> do
-      (cty, alrdsn', lts') <- toCType t [] lts names alrdsn False
+      (cty, alrdsn', lts') <- toCDecl t [] lts names alrdsn False
       return (CSpine {
-          shead = "(" ++ show cty ++ ")",   -- parenthésé : un seul argument
-          sargs = []
+          head = "(" ++ show cty ++ ")",
+          args = []
         }, alrdsn', lts')
     _ -> return (CSpine{
-            shead  = P.prettyShow t,
-            sargs  = []
+            head  = P.prettyShow t,
+            args  = []
       }, alrdsn , lts)
 
--- isImplicit :: Dom e  -> Bool
--- isImplicit d = (argInfoHiding $ domInfo d) /= NotHidden
-
-toCType  :: Term ->
-            [(String, Maybe CType)] ->
-            [(String, Maybe CType, [CRule])] ->
+toCDecl  :: Term ->
+            [CDecl] ->
+            [CDecl] ->
             [String] ->
             Map String Type ->
             Bool -> -- TopLevel ?
-            (TCM (CType, Map String Type, [(String, Maybe CType, [CRule])]))
-toCType t bds lts names alrdsn tplvl =
+            TCM (CDecl, Map String Type, [CDecl])
+toCDecl t bds lts names alrdsn tplvl =
   case t of
     Pi a (NoAbs nb b ) -> do
-      (domty, alrdsn', lts') <- (typeToCType (unDom a) [] lts names alrdsn) False -- (isImplicit a || prms > 0) 0) -- Convert domain type
-      typeToCType b ((nb , Just domty) : bds) lts' names alrdsn'  tplvl -- implicit (if prms > 0 then (prms  - 1) else 0)-- Convert  codomain type
+      (domty, alrdsn', lts') <- (typeToCDecl (unDom a) [] lts names alrdsn) False -- (isImplicit a || prms > 0) 0) -- Convert domain type
+      typeToCDecl b ((nb , Just domty) : bds) lts' names alrdsn'  tplvl -- implicit (if prms > 0 then (prms  - 1) else 0)-- Convert  codomain type
     Pi a b -> do
-      (domty, alrdsn', lts') <- typeToCType (unDom a) [] lts names alrdsn False -- (isImplicit a || prms > 0) 0
-      typeToCType (unAbs b) ((absName b, Just domty) : bds) lts' (absName b : names) alrdsn' tplvl  -- implicit (if prms > 0 then prms - 1 else 0)
+      (domty, alrdsn', lts') <- typeToCDecl (unDom a) [] lts names alrdsn False -- (isImplicit a || prms > 0) 0
+      typeToCDecl (unAbs b) ((absName b, Just domty) : bds) lts' (absName b : names) alrdsn' tplvl  -- implicit (if prms > 0 then prms - 1 else 0)
     Sort s -> do
-      (codom, alrdsns, lets) <- toCSpine t names lts alrdsn -- implicit       -- On defined names
-      return (CType {
-        bindings = reverse bds,
+      (spine, alrdsns, lets) <- toCSpine t names lts alrdsn -- implicit       -- On defined names
+      return (CExpr {
+        params = reverse bds,
         lets = if tplvl then reverse lts else [],
-        codom
+        spine
       }, alrdsns, lets)
     Def qname el -> do
-      (codom, alrdsns, lets) <- toCSpine t names lts alrdsn -- implicit       -- On defined names
-      return (CType {
-        bindings = reverse bds,
+      (spine, alrdsns, lets) <- toCSpine t names lts alrdsn -- implicit       -- On defined names
+      return (CExpr {
+        params = reverse bds,
         lets = if tplvl then reverse lets else [],
-        codom
+        spine
       }, alrdsns, lets )
     _ -> do
-      (codom, alrdsns, lets) <- toCSpine t names lts alrdsn  -- implicit    -- On defined names
-      return (CType {
-        bindings = reverse bds,
+      (spine, alrdsns, lets) <- toCSpine t names lts alrdsn  -- implicit    -- On defined names
+      return (CExpr {
+        params = reverse bds,
         lets = if tplvl then reverse lets else [],
-        codom
+        spine
       }, alrdsns , lets)
 
-elimsToCterm ::  [Elim' Term] -> [String] ->  [(String, Maybe CType, [CRule])] -> Map String Type -> TCM ([CTerm], Map String Type, [(String, Maybe CType, [CRule])])
-elimsToCterm e names lts alrdsn = -- pars =
+elimsToCExpr ::  [Elim' Term] -> [String] ->  [CDecl] -> Map String Type -> TCM ([CExpr], Map String Type, [CDecl])
+elimsToCExpr e names lts alrdsn =
   case e of
     [] -> return ([], alrdsn, lts)
     el : els -> do
-      (el', alrdsn', lts' ) <- elimToCterm el names lts alrdsn -- (pars > 0)
-      (els', alrdsn'', lts'') <- elimsToCterm els names lts' alrdsn' -- (pars - 1)
+      (el', alrdsn', lts' ) <- elimToCExpr el names lts alrdsn -- (pars > 0)
+      (els', alrdsn'', lts'') <- elimsToCExpr els names lts' alrdsn' -- (pars - 1)
       return (el' : els', alrdsn'' , lts'')
   where
 
-  elimToCterm  ::  Elim' Term -> [String] ->  [(String, Maybe CType, [CRule])] -> Map String Type -> TCM (CTerm, Map String Type, [(String, Maybe CType, [CRule])])
-  elimToCterm c names lts alrdsn = -- pars=
+  elimToCExpr  ::  Elim' Term -> [String] ->  [CDecl] -> Map String Type -> TCM (CExpr, Map String Type, [CDecl])
+  elimToCExpr c names lts alrdsn = -- pars=
     case c of
-      Apply t -> toCTerm (unArg t) names lts alrdsn  -- (pars || argInfoHiding (argInfo t) /= NotHidden)
-      e -> return (CTerm {
-          thead = [],
-          targs = CSpine {
-              shead = P.prettyShow e,
-              sargs = []
+      Apply t -> toCExpr (unArg t) names lts alrdsn  -- (pars || argInfoHiding (argInfo t) /= NotHidden)
+      e -> return (CExpr {
+          params = [],
+          lets = [],
+          spine = CSpine {
+              head = P.prettyShow e,
+              args = []
             }
         }, alrdsn, lts)
 
-toCTerm :: Term -> [String] -> [(String, Maybe CType, [CRule])] -> Map String Type -> TCM (CTerm, Map String Type, [(String, Maybe CType, [CRule])])
-toCTerm t names lts alrdsn = do
-  (targs, alrdsn', lets) <- toCSpine t names lts alrdsn
-  return (CTerm {
-    thead = [],
-    targs
+toCExpr :: Term -> [String] -> [CDecl] -> Map String Type -> TCM (CExpr, Map String Type, [CDecl])
+toCExpr t names lts alrdsn = do
+  (spine, alrdsn', lets) <- toCSpine t names lts alrdsn
+  return (CExpr{
+    params = [],
+    lets = [],
+    spine
   } , alrdsn', lets)
 
 getConstParams :: Definition -> Int
@@ -183,25 +182,25 @@ getConstParams d =
     ConstructorDefn c -> _conPars c
     _ -> __IMPOSSIBLE__
 
-gatherDatatypeInformations :: QName -> [(String, Maybe CType, [CRule])] -> Map String Type -> [String] -> TCM ([(String, Maybe CType, [CRule])], Map String Type)
+gatherDatatypeInformations :: QName -> [CDecl] -> Map String Type -> [String] -> TCM ([CDecl], Map String Type)
 gatherDatatypeInformations qn lts alrdsn names =
   if ((nameToString  qn) `member` alrdsn) then return (lts, alrdsn)
   else do
     def <- getConstInfo qn                                               -- gather its informations
     let ty = defType def                                                 -- get it's type
     let alrdsn' = insert (nameToString qn) ty alrdsn                                   -- we add the new encoutered type in the list
-    (tys, alr, letsss) <- (typeToCType ty [] lts names alrdsn' False )   -- convert it and add it to already seen types
-    let letss = (P.prettyShow $ qnameName qn , Just tys, []) : letsss
+    (tys, alr, letsss) <- (typeToCDecl ty [] lts names alrdsn' False )   -- convert it and add it to already seen types
+    let letss = tys : letsss
     case theDef def of                                                   -- match on the kind of definition we have for the type
       DatatypeDefn DatatypeData { _dataCons = cons } -> do               -- get all the type constructors names
         defs <- mapM getConstInfo cons                                   -- get their informations
         let tys = map (\d -> (defType d, getConstParams d)) defs
         let ald = foldl (\m (k, v) -> insert k v m ) alr (zip (map nameToString  cons) (map fst tys))
         (ctys, alrdsnes, lts'') <- foldlM (\(acc, alrdsn', lets) (t, p) -> do
-                                (nt, alrdsns', lets') <- typeToCType t [] lets names alrdsn' False -- False p -- (cpt + 1000)
+                                (nt, alrdsns', lets') <- typeToCDecl t [] lets names alrdsn' False -- False p -- (cpt + 1000)
                                 return (nt : acc, alrdsns', lets'))
                                 ([], ald, letss) tys
-        let lteess = (reverse $ (myZip   (map (P.prettyShow <$> qnameName) cons) (map Just (reverse ctys)) ) ) ++ lts''
+        let lteess = (reverse $ ctys) ++ lts''
         return (lteess, alrdsnes)
       _ -> return (letss , alr)
 
@@ -210,21 +209,20 @@ myZip [] [] = []
 myZip (a : as) (b : bs) = (a, b, []) : myZip as bs
 myZip _ _ = __IMPOSSIBLE__
 
-
-produceCanonicalGoal :: Telescope -> Type -> TCM (Map String Type, CType)
-produceCanonicalGoal ctx ty = aux ctx ty [(".path", Just topathType, [toPathRule1 , toPathRule2, toPathRule3]),(".mp", Just mpType, []), ("i1", Just $ simpleType "I", []), ("i0", Just $ simpleType "I", []),("I", Nothing, []), ("Set", Nothing, [])] [] [] mempty
+produceCanonicalGoal :: Telescope -> Type -> TCM CDecl
+produceCanonicalGoal ctx ty = aux ctx ty [CDecl "Set" Nothing [] ] [] [] mempty
   where
 
-  aux :: Telescope -> Type -> [(String, Maybe CType, [CRule])] -> [(String, Maybe CType)] -> [String] -> Map String Type ->  TCM (Map String Type, CType)
+  aux :: Telescope -> Type -> [CDecl] -> [CDecl] -> [String] -> Map String Type ->  TCM CDecl
   aux ctx ty revLets revPis names alrdsn =
     case ctx of
       EmptyTel -> do
-        (res , al, _) <- typeToCType ty revPis revLets names alrdsn True
-        return (al, res)
+        (res , al, _) <- typeToCDecl ty revPis revLets names alrdsn True
+        return res
       ExtendTel dom (Abs nb b) ->
         case unEl ty of
           Pi d codom -> do
-            (domty, alrdsns, lts') <- (typeToCType (unDom dom) [] revLets names alrdsn False)
+            (domty, alrdsns, lts') <- (typeToCDecl (unDom dom) [] revLets names alrdsn False)
             aux b (unAbs codom) ((nb, Just domty, []) : lts' ) revPis (nb : names) alrdsns
           _ -> __IMPOSSIBLE__
       _ -> __IMPOSSIBLE__
@@ -235,7 +233,7 @@ call_canonical norm ii rng args = do
     metaId <- lookupInteractionId ii
     getMetaTypeInContext metaId
   ctx <- liftTCM $ withInteractionId ii getContextTelescope
-  (al, goal) <- liftTCM $ (produceCanonicalGoal ctx ty)
+  goal <- liftTCM $ (produceCanonicalGoal ctx ty)
   liftIO $
     useAsCString (toStrict (encode goal)) $ \ety -> do -- may be dangerous, have to check
     -- name <- newCString "proof"
--- src/full/Agda/Canonical/Types.hs
+++ src/full/Agda/Canonical/Types.hs
@@ -7,30 +7,29 @@ import Data.List (intercalate)
 import Agda.Utils.Lens (set)
 import Agda.Utils.Impossible (__IMPOSSIBLE__)
 
-data CRule = CRule
-  { rlhs :: CSpine,
-    rrhs :: CSpine
+data CEquation = CEquation
+  { lhs :: CSpine,
+    rhs :: CSpine
   }
   deriving (Generic)
 
 data CSpine = CSpine
-  { shead :: String,
-    sargs :: [CTerm]
+  { head :: String,
+    args :: [CExpr]
   }
   deriving (Generic)
 
-
-data CTerm = CTerm
-  { thead :: [String],
-    targs :: CSpine
+data CDecl = CDecl {
+    name :: String,
+    typ  :: Maybe CExpr,
+    equations :: [CEquation]
   }
   deriving (Generic)
 
-
-data CType = CType
-  { bindings :: [(String, Maybe CType)],
-    lets :: [(String, Maybe CType, [CRule])],
-    codom :: CSpine
+data CExpr = CExpr
+  { params :: [CDecl],
+    lets :: [CDecl],
+    spine :: CSpine
   }
   deriving (Generic)
 
@@ -55,108 +54,88 @@ instance Show CSpine where
   --     aux [t] = " " ++ show t
   --     aux (t : l) = " " ++ show t ++ aux l
 
+instance FromJSON CDecl where
+  parseJSON = withObject "CDecl" $
+    \v ->
+      CDecl
+        <$> v .: "name"
+        <*> v .: "typ"
+        <*> v .: "equations"
+
+instance ToJSON CDecl where
+  toEncoding = genericToEncoding defaultOptions
+
 instance FromJSON CSpine where
   parseJSON = withObject "CSpine" $
     \v ->
       CSpine
-        <$> v .: "shead"
-        <*> v .: "sargs"
+        <$> v .: "head"
+        <*> v .: "args"
 
 instance ToJSON CSpine where
   toEncoding = genericToEncoding defaultOptions
 
-instance FromJSON CRule where
-  parseJSON = withObject "CRule" $
-    \v ->
-      CRule
-        <$> v .: "rlhs"
-        <*> v .: "rrhs"
 
-instance ToJSON CRule where
+instance FromJSON CEquation where
+  parseJSON = withObject "CEquation" $
+    \v ->
+      CEquation
+        <$> v .: "lhs"
+        <*> v .: "rhs"
+
+instance ToJSON CEquation where
   toEncoding = genericToEncoding defaultOptions
 
-
-instance Show CTerm where
-  showsPrec p (CTerm th ta) =
-    let lamPrec = 0  in
-    showParen (p > lamPrec && not (null th)) $
-      case th of
-        [] -> showsPrec p ta
-        _ -> showString "λ "
-             . showString (unwords th)
-             . showString " → "
-             . showsPrec lamPrec ta
-
-{-
-  show (CTerm {thead = th, targs = ta}) =
-    case th of
-      [] -> show ta
-      _ -> "(" ++ pplbd th ++ show ta ++ ")"
-    where
-      pplbd :: [String] -> String
-      pplbd [] = ""
-      pplbd b = "λ " ++ aux b ++ " → "
-        where
-          aux :: [String] -> String
-          aux [] = ""
-          aux [s] = s
-          aux (s : l) = s ++ ", " ++ aux l
--}
-instance FromJSON CTerm where
-  parseJSON = withObject "CTerm" $
+instance FromJSON CExpr where
+  parseJSON = withObject "CExpr" $
     \v ->
-      CTerm
-        <$> v .: "thead"
-        <*> v .: "targs"
-
-instance ToJSON CTerm where
-  toEncoding = genericToEncoding defaultOptions
-
-instance Show CRule where
-  show CRule{rlhs , rrhs} = show rlhs ++ " ⤇ " ++ show rrhs
-
-
-instance Show CType where
-  show (CType {bindings = bds, lets = lts, codom = sp}) = ppbds bds ++ pplts lts ++ show sp
-    where
-      ppbds :: [(String, Maybe CType)] -> String
-      ppbds [] = ""
-      ppbds b = "Π" ++ aux b ++ ". "
-        where
-          aux :: [(String, Maybe CType)] -> String
-          aux [] = ""
-          aux [(s, t)] = "(" ++ s ++ " : " ++ show t ++ ")"
-          aux ((s, t) : l) = "(" ++ s ++ " : " ++ show t ++ "), " ++ aux l
-
-      pplts :: [(String, Maybe CType, [CRule])] -> String
-      pplts [] = ""
-      pplts b = "let " ++ aux b ++ ". "
-        where
-          auxaux :: [CRule] -> String
-          auxaux [] = ""
-          auxaux l = "{" ++ auxauxaux l ++ "}"
-            where
-              auxauxaux :: [CRule] -> String
-              auxauxaux rs = case rs of
-                [] -> __IMPOSSIBLE__
-                [r] -> show r
-                r : rs -> show r ++ ", " ++ auxauxaux rs
-
-          aux :: [(String, Maybe CType, [CRule])] -> String
-          aux [] = ""
-          aux [(s, t, rs)] = "(" ++ s ++ " : " ++ show t ++ " " ++ (auxaux rs) ++ ")"
-          aux ((s, t, rs) : l) = "(" ++ s ++ " : " ++ show t ++ " " ++ auxaux rs ++ "), " ++ aux l
-
-instance ToJSON CType where
-  toEncoding = genericToEncoding defaultOptions
-
-instance FromJSON CType where
-  parseJSON = withObject "CType" $
-    \v ->
-      CType
-        <$> v .: "bindings"
+      CExpr
+        <$> v .: "params"
         <*> v .: "lets"
-        <*> v .: "codom"
+        <*> v .: "spine"
+
+instance ToJSON CExpr where
+  toEncoding = genericToEncoding defaultOptions
+
+
+
+instance Show CEquation where
+  show CEquation{lhs , rhs} = show lhs ++ " ⤇ " ++ show rhs
+
+instance Show CDecl where
+  show CDecl{name , typ, equations} = "(" ++ name ++ ":" ++ show typ ++ "[" ++ show  equations ++ "]"
+
+instance Show CExpr where
+  show (CExpr {params, lets, spine}) = "Π" ++ show params  ++ ". let " ++ show  lets ++ show spine
+    -- where
+    --   ppbds :: [(String, Maybe CType)] -> String
+    --   ppbds [] = ""
+    --   ppbds b = "Π" ++ aux b ++ ". "
+    --     where
+    --       aux :: [(String, Maybe CType)] -> String
+    --       aux [] = ""
+    --       aux [(s, t)] = "(" ++ s ++ " : " ++ show t ++ ")"
+    --       aux ((s, t) : l) = "(" ++ s ++ " : " ++ show t ++ "), " ++ aux l
+    --
+    --   pplts :: [(String, Maybe CType, [CRule])] -> String
+    --   pplts [] = ""
+    --   pplts b = "let " ++ aux b ++ ". "
+    --     where
+    --       auxaux :: [CRule] -> String
+    --       auxaux [] = ""
+    --       auxaux l = "{" ++ auxauxaux l ++ "}"
+    --         where
+    --           auxauxaux :: [CRule] -> String
+    --           auxauxaux rs = case rs of
+    --             [] -> __IMPOSSIBLE__
+    --             [r] -> show r
+    --             r : rs -> show r ++ ", " ++ auxauxaux rs
+    --
+    --       aux :: [(String, Maybe CType, [CRule])] -> String
+    --       aux [] = ""
+    --       aux [(s, t, rs)] = "(" ++ s ++ " : " ++ show t ++ " " ++ (auxaux rs) ++ ")"
+    --       aux ((s, t, rs) : l) = "(" ++ s ++ " : " ++ show t ++ " " ++ auxaux rs ++ "), " ++ aux l
+
 
 data CanonicalResult
   = CanonicalExpr String
@@ -166,136 +145,136 @@ data CanonicalResult
 
 dummyCSpine :: CSpine
 dummyCSpine = CSpine {
-    shead = "",
-    sargs = []
+    head = "",
+    args = []
   }
 
-dummyCTerm :: CTerm
-dummyCTerm = CTerm {
-    thead = [],
-    targs = dummyCSpine
-  }
+-- dummyCTerm :: CTerm
+-- dummyCTerm = CTerm {
+--     thead = [],
+--     targs = dummyCSpine
+--   }
 
-dummyCType :: CType
-dummyCType = CType {
-    bindings = [],
+dummyCExpr :: CExpr
+dummyCExpr = CExpr {
+    params = [],
     lets = [],
-    codom = dummyCSpine
+    spine = dummyCSpine
   }
-
-simpleSpine :: String -> CSpine
-simpleSpine s = CSpine {
-      shead = s,
-      sargs = []
-}
-
-simpleTerm :: String -> CTerm
-simpleTerm s =
-  CTerm {
-    thead = [],
-    targs = simpleSpine s
-  }
-
-simpleType :: String -> CType
-simpleType s = CType {
-  bindings = [],
-  lets = [],
-  codom = simpleSpine s
-}
-
-pathSpine :: CSpine
-pathSpine =
-  CSpine {
-    shead = "_≡_",
-    sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y"]
-  }
-
-mpType :: CType
-mpType =
-  let tS = simpleType "Set ℓ"
-      tI = simpleType "I"
-      tA = simpleType "A"
-      tL = simpleType "Level"
-      tp = CType {
-          bindings = [ ("i", Just tI)],
-          lets = [],
-          codom = simpleSpine "A"
-        }
-      codom = pathSpine  in
-  CType {
-    bindings = [("ℓ", Just tL), ("A", Just tS), ("p", Just tp), ("x", Just tA), ("y", Just tA)],
-    lets = [],
-    codom
-  }
-
-
-topathType :: CType
-topathType =
-  let tS = simpleType "Set ℓ"
-      tI = simpleType "I"
-      tA = simpleType "A"
-      tL = simpleType "Level"
-      tP = CType {
-          bindings = [],
-          lets = [],
-          codom = pathSpine
-        }
-  in
-  CType{
-    bindings = [("ℓ", Just tL), ("A", Just tS), ("x", Just tA), ("y", Just tA), ("p", Just tP), ("i", Just tI)],
-    lets = [],
-    codom = simpleSpine "A"
-  }
-
-mpSpine :: CSpine
-mpSpine  = CSpine {
-             shead = ".mp",
-             sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "p", simpleTerm "x", simpleTerm "y"]
-           }
-
-mpTerm :: CTerm
-mpTerm  = CTerm {
-            thead = [],
-            targs = mpSpine
-          }
-
-toPathRule1 :: CRule
-toPathRule1 =
-  let rlhs = CSpine {
-              shead = ".path",
-              sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y", mpTerm ]
-            }
-      rrhs = simpleSpine "p"
-  in
-  CRule {
-      rlhs,
-      rrhs
-  }
-
-
-toPathRule2 :: CRule
-toPathRule2 =
-  let rlhs = CSpine {
-              shead = ".path",
-              sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y", mpTerm, simpleTerm "i0"]
-            }
-      rrhs = simpleSpine "x"
-  in
-  CRule {
-      rlhs,
-      rrhs
-  }
-
-
-toPathRule3 :: CRule
-toPathRule3 =
-  let rlhs = CSpine {
-              shead = ".path",
-              sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y", mpTerm, simpleTerm "i1"]
-            }
-      rrhs = simpleSpine "y"
-  in
-  CRule {
-      rlhs,
-      rrhs
-  }
+--
+-- simpleSpine :: String -> CSpine
+-- simpleSpine s = CSpine {
+--       shead = s,
+--       sargs = []
+-- }
+--
+-- simpleTerm :: String -> CTerm
+-- simpleTerm s =
+--   CTerm {
+--     thead = [],
+--     targs = simpleSpine s
+--   }
+--
+-- simpleType :: String -> CType
+-- simpleType s = CType {
+--   bindings = [],
+--   lets = [],
+--   codom = simpleSpine s
+-- }
+--
+-- pathSpine :: CSpine
+-- pathSpine =
+--   CSpine {
+--     shead = "_≡_",
+--     sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y"]
+--   }
+--
+-- mpType :: CType
+-- mpType =
+--   let tS = simpleType "Set ℓ"
+--       tI = simpleType "I"
+--       tA = simpleType "A"
+--       tL = simpleType "Level"
+--       tp = CType {
+--           bindings = [ ("i", Just tI)],
+--           lets = [],
+--           codom = simpleSpine "A"
+--         }
+--       codom = pathSpine  in
+--   CType {
+--     bindings = [("ℓ", Just tL), ("A", Just tS), ("p", Just tp), ("x", Just tA), ("y", Just tA)],
+--     lets = [],
+--     codom
+--   }
+--
+--
+-- topathType :: CType
+-- topathType =
+--   let tS = simpleType "Set ℓ"
+--       tI = simpleType "I"
+--       tA = simpleType "A"
+--       tL = simpleType "Level"
+--       tP = CType {
+--           bindings = [],
+--           lets = [],
+--           codom = pathSpine
+--         }
+--   in
+--   CType{
+--     bindings = [("ℓ", Just tL), ("A", Just tS), ("x", Just tA), ("y", Just tA), ("p", Just tP), ("i", Just tI)],
+--     lets = [],
+--     codom = simpleSpine "A"
+--   }
+--
+-- mpSpine :: CSpine
+-- mpSpine  = CSpine {
+--              shead = ".mp",
+--              sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "p", simpleTerm "x", simpleTerm "y"]
+--            }
+--
+-- mpTerm :: CTerm
+-- mpTerm  = CTerm {
+--             thead = [],
+--             targs = mpSpine
+--           }
+--
+-- toPathRule1 :: CRule
+-- toPathRule1 =
+--   let rlhs = CSpine {
+--               shead = ".path",
+--               sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y", mpTerm ]
+--             }
+--       rrhs = simpleSpine "p"
+--   in
+--   CRule {
+--       rlhs,
+--       rrhs
+--   }
+--
+--
+-- toPathRule2 :: CRule
+-- toPathRule2 =
+--   let rlhs = CSpine {
+--               shead = ".path",
+--               sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y", mpTerm, simpleTerm "i0"]
+--             }
+--       rrhs = simpleSpine "x"
+--   in
+--   CRule {
+--       rlhs,
+--       rrhs
+--   }
+--
+--
+-- toPathRule3 :: CRule
+-- toPathRule3 =
+--   let rlhs = CSpine {
+--               shead = ".path",
+--               sargs = [simpleTerm "ℓ", simpleTerm "A", simpleTerm "x", simpleTerm "y", mpTerm, simpleTerm "i1"]
+--             }
+--       rrhs = simpleSpine "y"
+--   in
+--   CRule {
+--       rlhs,
+--       rrhs
+--   }
